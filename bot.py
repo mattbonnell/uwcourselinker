622cee5993b9af8c92a53bc4143ff5bb533b97a2
mattbonnell <matt.bonnell@icloud.com>
2017-10-26 15:26:59 -0400

    Functional overhaul

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ subreddit = bot.subreddit('uwaterloo')
 
 comments = subreddit.stream.comments()
 
-courseCode = r'[A-Z]{2,4} [0-9]{3}'
+courseCode = r'[A-z]{2,4} ?[0-9]{3}'
 
 context = ssl._create_unverified_context()
 
@@ -27,46 +27,61 @@ seen_submissions = []
 
 SELF = "UWCourseLinker"
 
-while True:
-    try:
-        for comment in subreddit.comments():
-            if comment.author is not None:
-                text = comment.body
-                author = comment.author
-                if str(author.name) != SELF:
-                    codes = re.findall(courseCode, text)
-                    message = ""
-                    if len(codes) > 0:
-                        if comment.id not in seen_comments:
-                            seen_comments.append(comment.id)
-                            seen_comments_file.write(comment.id + "\n")
-                            seen_codes = []
-                            for code in codes:
-                                if code not in seen_codes:
-                                    print(code)
-                                    seen_codes.append(code)
-                                    clean_code = code
-                                    "".join(clean_code.split())
-                                    clean_code = clean_code.replace(" ", "")
-                                    clean_code = clean_code.lower()
-                                    url = 'https://uwflow.com/course/' + clean_code
-                                    with urllib.request.urlopen(url, context=context) as response:
-                                        html = response.read()
-                                        title = str(re.search(r'<title>[\s\S]+<\/title>', str(html)))
-                                        name = title[title.find(code):title.rfind("-") - 1]
-                                    message += "[" + name + "](" + url + ")\n\n"
-                            postedSuccessfully = False
-                            while not postedSuccessfully:
-                                try:
-                                    comment.reply(message)
-                                    postedSuccessfully = True
-                                except Exception as e:
-                                    print("Exception:", e)
-                                    print("Sleeping for 1 minute...")
-                                    time.sleep(60)
 
-    except KeyboardInterrupt:
-        seen_comments_file.close()
-        break
-    except Exception as e:
-        print("Exception:", e)
+def get_name_and_url(course_code):
+    clean_code = course_code
+    "".join(clean_code.split())
+    clean_code = clean_code.replace(" ", "")
+    clean_code = clean_code.lower()
+    url = 'https://uwflow.com/course/' + clean_code
+    with urllib.request.urlopen(url, context=context) as response:
+        html = response.read()
+        title = str(re.search(r'<title>[\s\S]+<\/title>', str(html)))
+        name = title[title.find(course_code):title.rfind("-") - 1]
+    return name, url
+
+
+def bot():
+    while True:
+        try:
+            for comment in subreddit.comments():
+                if comment.author is not None:
+                    text = comment.body
+                    author = comment.author
+                    if str(author.name) != SELF:
+                        codes = re.findall(courseCode, text)
+                        message = ""
+                        if len(codes) > 0:
+                            if comment.id not in seen_comments:
+                                seen_comments.append(comment.id)
+                                seen_comments_file.write(comment.id + "\n")
+                                seen_codes = []
+                                for code in codes:
+                                    if code not in seen_codes:
+                                        print(code)
+                                        seen_codes.append(code)
+                                        try:
+                                            name, url = get_name_and_url(code)
+                                            message += "[" + name + "](" + url + ")\n\n"
+                                        except Exception as e:
+                                            print("Exception:", e)
+                                            continue
+
+                                posted_successfully = False
+                                while not posted_successfully:
+                                    try:
+                                        comment.reply(message)
+                                        posted_successfully = True
+                                    except Exception as e:
+                                        print("Exception:", e)
+                                        print("Sleeping for 1 minute...")
+                                        time.sleep(60)
+
+        except KeyboardInterrupt:
+            seen_comments_file.close()
+            break
+        except Exception as e:
+            print("Exception:", e)
+
+if __name__ == '__main__':
+    main()
